4d9b043be94679397708a9536e8d904faec227c2
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-09-26 01:10:20 +0530

    Initial commit

--- src/players/SnakesAndLadders.java
+++ src/players/SnakesAndLadders.java
@@ -1,8 +1,16 @@
 package players;
 
 public class SnakesAndLadders {
+	static int count1 = 0;
+	static int count2 = 0;
+	static int pos1 = 1 ;
+	static int pos2 = 1 ; 
+	static String p1 = "Player1";
+	static String p2 = "Player2";
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snakes and Ladders");
+		
 	}
+		
 
 }
